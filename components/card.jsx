15e38c6353afd09f7228023070ff57a0ff1bca03
Battugs Bayanbileg <battugs@Battugss-MacBook-Air.local>
2023-12-12 17:45:51 +0800

    asd

---
+++ components/card.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+export default function Home() {
+  return (
+    <div>
+      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2x  ">
+        <div class="md:flex">
+          <div class="md:shrink-0">
+            <div
+              class="h-48 w-full object-cover md:h-full md:w-48 bg-sky-500"
+              src="https://nestmoodle.xyz/pluginfile.php/1/core_admin/logocompact/300x300/1698062409/NHS.d046134c.png"
+              alt="Modern building architecture"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Company retreats
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Incredible accommodation for your team
+            </a>
+            <p class="mt-2 text-slate-500">
+              Looking to take your team away on a retreat to enjoy awesome food
+              and take in some sunshine? We have a list of places to do just
+              that.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
+import React from 'react';
+
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div class="grid grid-rows-4 grid-flow-col gap-4">
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
-        <div>04</div>
-        <div>05</div>
-        <div>06</div>
-        <div>07</div>
-        <div>08</div>
-        <div>09</div>
-        <div></div>
-      </div>
-    </main>
+    <>
+      <div>asd</div>
+    </>
   );
 }
